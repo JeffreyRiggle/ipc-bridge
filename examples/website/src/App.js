5a87b9b6c260f9826105463ce7b5d82ec7410242
Jeff <JeffreyRiggle@gmail.com>
2022-05-10 05:46:43 -0400

    chore(example-app): Upgrade dependencies

--- examples/website/src/App.js
+++ examples/website/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {client} from '@jeffriggle/ipc-bridge-client';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
